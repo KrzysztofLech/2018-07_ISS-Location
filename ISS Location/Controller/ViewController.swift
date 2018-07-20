4c0f236f22381a85ea40a24a63ec39c285438c74
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-07-20 15:23:11 +0200

    Timer added

--- ISS Location/Controller/ViewController.swift
+++ ISS Location/Controller/ViewController.swift
@@ -17,12 +17,23 @@ class ViewController: UIViewController {
     private var issPosition: IssPosition?
     private var issCrew: [CrewMan] = []
     
+    private let timeInterval: Double = 5.0
+    
+    // MARK: - Life cycle methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getCurrentPosition()
         getCrewInfo()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupTimer()
+    }
+    
+    // MARK: - API data methods
     
     private func getCurrentPosition() {
         RequestManager.getIssPositionData { (position) in
@@ -36,16 +47,22 @@ class ViewController: UIViewController {
             self.issCrew = crew
         }
     }
+    
+    // MARK: - View refreshing methods
 
     private func refreshView() {
         guard let issPosition = issPosition else { return }
         
         lastTimeLabel.text = Helper.getDateFromTimestamp(issPosition.timestamp)
-        print(issPosition.position.latitude)
-        print(issPosition.position.longitude)
         
         let position = String(format: "latitude: %@ / longitude: %@", issPosition.position.latitude, issPosition.position.longitude)
         lastPositionLabel.text = position
     }
+    
+    // MARK: - Timer methods
+    private func setupTimer() {
+        Timer.scheduledTimer(withTimeInterval: timeInterval, repeats: true, block: { [weak self] _ in
+            self?.getCurrentPosition()
+        })
+    }
 }
-
